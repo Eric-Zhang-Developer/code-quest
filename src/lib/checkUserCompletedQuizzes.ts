e0b43f5e343641136d7cb124753b1e6f71143002
Eric Zhang <superezzhang@gmail.com>
2025-11-23 21:53:41 -0500

    feat: add checkUserCompletedQuizzes.ts

---
+++ src/lib/checkUserCompletedQuizzes.ts
@@ -0,0 +1,25 @@
+import { createClient } from "./supabase/server";
+
+export default async function checkUserCompletedQuizzes() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    console.error("User not logged in, cannot save progress.");
+    return new Set();
+  }
+
+  const { data: quizData, error: selectError } = await supabase
+    .from("user_quiz_progress")
+    .select("quiz_id")
+    .eq("user_id", user.id);
+
+  if (selectError) {
+    console.error(`Supabase selection error: ${selectError.message}`);
+    return new Set();
+  }
+
+  return new Set(quizData.map((row) => row.quiz_id));
+}
