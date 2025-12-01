5c54c552e3a54ce86a51551449db176409d61952
Eric Zhang <superezzhang@gmail.com>
2025-12-01 12:36:02 -0500

    fix: hardcode website link

--- src/lib/supabase/handle-oauth-login.ts
+++ src/lib/supabase/handle-oauth-login.ts
@@ -3,18 +3,11 @@
 import { redirect } from "next/navigation";
 import { createClient } from "./server";
 
-// 1. Create a helper to get the REAL base URL
-const getURL = () => {
-  let url = process.env.NEXT_PUBLIC_SITE_URL ?? process.env.VERCEL_URL ?? "http://localhost:3000";
-
-  url = url.includes("http") ? url : `https://${url}`;
-  url = url.replace(/\/$/, "");
-  return url;
-};
-
 export default async function handleOAuthLogin(provider: "github" | "google") {
   const supabase = await createClient();
-  const origin = getURL();
+
+  const origin = "https://intro-swe-term-project.vercel.app";
+
   const { error, data } = await supabase.auth.signInWithOAuth({
     provider: provider,
     options: {
