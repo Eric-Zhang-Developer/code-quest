d405805eeb17d72fc226d4f00ae757d9613ac0ed
Eric Zhang <superezzhang@gmail.com>
2025-12-01 12:26:40 -0500

    fix: OAuth url redirect

--- src/lib/supabase/handle-oauth-login.ts
+++ src/lib/supabase/handle-oauth-login.ts
@@ -2,14 +2,24 @@
 
 import { redirect } from "next/navigation";
 import { createClient } from "./server";
-import { headers } from "next/headers";
+
+// 1. Create a helper to get the REAL base URL
+const getURL = () => {
+  let url = process.env.NEXT_PUBLIC_SITE_URL ?? process.env.VERCEL_URL ?? "http://localhost:3000";
+
+  url = url.includes("http") ? url : `https://${url}`;
+  url = url.replace(/\/$/, "");
+  return url;
+};
+
 export default async function handleOAuthLogin(provider: "github" | "google") {
   const supabase = await createClient();
-  const headersList = await headers();
-  const origin = headersList.get("origin") ?? `https://${headersList.get("host")}`;;
+  const origin = getURL();
   const { error, data } = await supabase.auth.signInWithOAuth({
     provider: provider,
-    options: { redirectTo: `${origin}/auth/callback?next=/dashboard` },
+    options: {
+      redirectTo: `${origin}/auth/callback?next=/dashboard`,
+    },
   });
 
   if (error) throw error;
